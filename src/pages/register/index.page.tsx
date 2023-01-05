ef40046924ac322a728ee98fc57bcd966652996c
Ednan <ednanjhony@yahoo.com.br>
2023-01-05 07:01:26 -0300

    added cookies

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,7 +5,10 @@ import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../lib/axios'
 import { Container, Form, FormError, Header } from './styles'
+
+import AxiosError from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -41,7 +44,19 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.log(err)
+    }
   }
 
   return (
