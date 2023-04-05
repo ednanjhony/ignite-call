368c2c614164fcbfcde9ff3f7e2de14f3dc17a78
Ednan <ednanjhony@yahoo.com.br>
2023-04-05 08:03:47 -0300

    doing availability time

---
+++ src/pages/schedule/username/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,68 @@
+import dayjs from 'dayjs'
+import { useEffect, useState } from 'react'
+import { Calendar } from '../../../../../components/Calendar'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
+import { api } from '../../../../../lib/axios'
+
+export function CalendarStep() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [availability, setAvailability] = useState(null)
+
+  const isDateSelected = !!selectedDate
+  const username = String(router.query.username)
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
+
+  useEffect(() => {
+    if (!selectedDate) {
+      return
+    }
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+      })
+  }, [selectedDate, username])
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            {weekDay} <span>{describedDate}</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>14:00h</TimePickerItem>
+            <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>16:00h</TimePickerItem>
+            <TimePickerItem>17:00h</TimePickerItem>
+            <TimePickerItem>18:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
